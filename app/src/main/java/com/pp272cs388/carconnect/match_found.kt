3bfd63821ca18d93373026230e755645a5bb3489
palina-pauliuchenka <ppauliuchenka02@gmail.com>
2024-12-15 18:46:56 -0500

    Added functionality to buttons on the match-found-screen

--- app/src/main/java/com/pp272cs388/carconnect/match_found.kt
+++ app/src/main/java/com/pp272cs388/carconnect/match_found.kt
@@ -1,9 +1,12 @@
 package com.pp272cs388.carconnect
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.view.animation.BounceInterpolator
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class match_found : AppCompatActivity() {
@@ -11,6 +14,22 @@ class match_found : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_found)
+
+        // Buttons functionality
+        val acceptButton = findViewById<Button>(R.id.accept_button)
+        val denyButton = findViewById<Button>(R.id.deny_button)
+        // Let's first figure out the button update sequence
+        acceptButton.setOnClickListener {
+            val intent = Intent(this, main_screen::class.java)
+            Toast.makeText(this, "Accepted Ride!", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+        denyButton.setOnClickListener {
+            val intent = Intent(this, main_screen::class.java)
+            Toast.makeText(this, "Rejected the Ride!", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+
 
         val matchTextView: TextView = findViewById(R.id.matchTextView)
         animateTextView(matchTextView)
